75d3df5349411933c7bdb11433560be7ba59ccba
Lucia_Quagliata <lucia.quagliata5@gmail.com>
2025-03-17 20:23:34 -0300

    added info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (latitude, longitude, callback) => {
             } else{
                 //const temperature = response.body.current.temperature
                 //const feelslike = response.body.current.feelslike
-                callback(undefined, body.current.weather_descriptions[0] + '. It is ' + body.current.temperature + ' degrees today, and it feels like ' + body.current.feelslike)
+                callback(undefined, body.current.weather_descriptions[0] + '. It is ' + body.current.temperature + ' degrees today, and it feels like ' + body.current.feelslike + '. UV index is ' + body.current.uv_index)
             }
             
         }
