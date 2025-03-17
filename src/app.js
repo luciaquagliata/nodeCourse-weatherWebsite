5e814eb858bb944a0a8337e9d25c10daa31931ac
Lucia_Quagliata <lucia.quagliata5@gmail.com>
2025-03-17 20:16:16 -0300

    add about text

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const forecast = require('./utils/forecast.js')
 
 const app = express()
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
